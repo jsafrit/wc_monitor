4bf9ed25ed40f463943a3dd3607a0b96bc38c1e4
John Safrit <jsafrit@users.noreply.github.com>
2014-08-07 14:38:51 -0400

    Added looping to main.

--- wc_monitor.py
+++ wc_monitor.py
@@ -6,21 +6,18 @@ snapurl = 'http://dl0414.myfoscam.org/login_user.htm'
 #userauth = ('user1', 'pw1')
 
 
+while True:
+    r = requests.get(snapurl) #, auth=userauth)
+    soup = BeautifulSoup(r.content)
 
+    cam_up = len(soup.find_all('input', attrs={'id':'login_user'}))
 
-r = requests.get(snapurl) #, auth=userauth)
-soup = BeautifulSoup(r.content)
-
-#print r.content
-#print soup
-cam_up = len(soup.find_all('input', attrs={'id':'login_user'}))
-
-if not cam_up:
-    #send warning
-    print '\nCamera not responding! Sending message...'
-else:
-    print '.',
-    time.sleep(6)
+    if not cam_up:
+        #send warning
+        print '\nCamera not responding! Sending message...'
+    else:
+        print '.'
+        time.sleep(6)
     
 
 
