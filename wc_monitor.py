7fa224bcd8df94e622323e833d1ad090b80c5a30
John Safrit <jsafrit@users.noreply.github.com>
2014-08-07 16:26:06 -0400

    Added mail feature and minor refactoring.

--- wc_monitor.py
+++ wc_monitor.py
@@ -1,23 +1,55 @@
 import requests
 import time
 from bs4 import BeautifulSoup
+from SendMeMail import Mail
 
-snapurl = 'http://dl0414.myfoscam.org/login_user.htm'
-#userauth = ('user1', 'pw1')
+#####################################
+## user variables
+#####################################
+camurl = 'http://dl0414.myfoscam.org'
+#email = '3013797664@txt.att.net'
+email = '3362631393@vtext.com'
+checkInterval = 600  #in seconds
+retryLimit = 5
 
-
-while True:
-    r = requests.get(snapurl) #, auth=userauth)
+def getCamStatus():
+    '''Checks status of camera and responds True if reachable'''
+    r = requests.get(camurl + '/login_user.htm')
     soup = BeautifulSoup(r.content)
+    return bool(len(soup.find_all('input', attrs={'id':'login_user'})))
 
-    cam_up = len(soup.find_all('input', attrs={'id':'login_user'}))
-
-    if not cam_up:
-        #send warning
-        print '\nCamera not responding! Sending message...'
-    else:
-        print '.'
-        time.sleep(6)
+def sendNotification(mailer,cnt):
+    '''Send e-mail/text notification of failure'''
+    msg = 'Your remote camera could not be reached. (try #%d)' % cnt
+    print msg, 
+    mailer.setMessage(msg)
+    mailer.send()
     
 
+def main():
+    mailer = Mail(toaddr=email, subject='Camera Monitor')
+    warnCnt = 0
+    
+    while True:
+        cam_up = getCamStatus()
+
+        if not cam_up:
+            warnCnt += 1
+            #send a notificaion
+            sendNotification(mailer, warnCnt)
+            #exit before flooding
+            if warnCnt >= retryLimit:
+                break
+            #retry sooner since we failed
+            time.sleep(checkInterval/10)
+
+        else:
+            #reset warnings
+            warnCnt = 0
+            #wait for defined interval before checking again
+            time.sleep(checkInterval)
+
+
+if __name__ == '__main__':
+    main()
 
